dde6d370491c348f1ea16228a49620cffe7494c3
Pranjali Gupta <146095248+pranjaligupta0204@users.noreply.github.com>
2025-12-03 22:10:33 +0530

    Time: 0 ms (100.00%) | Memory: 8.7 MB (13.49%) - LeetSync

--- 50-powx-n/powx-n.cpp
+++ 50-powx-n/powx-n.cpp
@@ -1,19 +1,22 @@
 class Solution {
 public:
-    double myPow(double x, int n) {
-        long long p = n;
-        if (p < 0) {
-            x = 1 / x;
-            p = -p;
+    double myPow(double x, long long n) {
+        if(n == 0) return 1.0;
+
+        // If power is negative, convert it and take reciprocal later
+        bool isNegative = (n < 0);
+        n = llabs(n);
+
+        double result = 1.0;
+
+        while(n > 0){
+            if(n & 1)   // If last bit of n is 1, multiply result
+                result *= x;
+
+            x = x * x;  // Square the base
+            n >>= 1;    // Divide power by 2
         }
 
-        double ans = 1;
-        while (p > 0) {
-            if (p % 2 == 1)
-                ans *= x;
-            x *= x;
-            p /= 2;
-        }
-        return ans;
+        return isNegative ? 1.0 / result : result;
     }
 };
