e18be418d304bb759fe507d0ad1cb66c0adea02f
Pranjali Gupta <146095248+pranjaligupta0204@users.noreply.github.com>
2025-12-29 21:53:22 +0530

    Time: 18 ms (80.44%) | Memory: 59.6 MB (98.98%) - LeetSync

---
+++ 2263-maximum-running-time-of-n-computers/maximum-running-time-of-n-computers.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    long long maxRunTime(int n, vector<int>& batteries) {
+        long long low = 0;
+        long long high = 0;
+
+        for (int b : batteries)
+            high += b;
+
+        high /= n;  
+
+        long long ans = 0;
+
+        while (low <= high) {
+            long long mid = (low + high) / 2;
+
+            long long total = 0;
+            for (int b : batteries)
+                total += min((long long)b, mid);
+
+            if (total >= mid * n) {
+                ans = mid;
+                low = mid + 1;
+            } else {
+                high = mid - 1;
+            }
+        }
+
+        return ans;
+    }
+};
